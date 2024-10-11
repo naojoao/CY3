67ede2daf872a5a0e3792c33645a574da5864625
Gui2010ggg <madu.inacio.2005@gmail.com>
2024-10-11 17:32:34 -0300

    Aula 1

---
+++ aula-1/script.js
@@ -0,0 +1,22 @@
+// variáveis - const e let
+// const - vem de constante, não tem como alterar ela
+// let - é possivel alterar
+
+//const nome = "João"
+//let idade = 18
+
+//idade = 20
+
+//tipos de dados
+/* String, Number e Boolean - Primitivos
+    String - Textos
+    Number - Números
+    Boolean - Verdadeiro ou Falso (true or false)
+*/
+
+const nome = "Olf"
+const idade = 19
+const altura = 1.80
+const GuilhermeVeio = true
+
+console.log(nome, idade, altura, GuilhermeVeio)
